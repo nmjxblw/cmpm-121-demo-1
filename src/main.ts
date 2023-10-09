1a15fdb625e0eaf54c6e68f2d4a3a6916946a295
nmjxblw <zchen386@ucsc.edu>
2023-10-09 14:59:52 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const emoji_buttom = document.createElement("buttom");
+emoji_buttom.innerHTML = "😃";
+let counter: number = 0;
+emoji_buttom.addEventListener("click", () => {
+  counter++;
+  emoji_buttom.innerHTML = `😃+${counter}`;
+});
+app.append(emoji_buttom);
