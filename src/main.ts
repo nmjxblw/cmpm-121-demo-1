579a3c40fa77d6afa18b709db646e326f5854db0
nmjxblw <zchen386@ucsc.edu>
2023-10-09 18:46:35 -0700

    step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,22 @@ const gameName = "Zhuo's game";
 
 document.title = gameName;
 
+//step1
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//step2
 const emoji_buttom = document.createElement("buttom");
 emoji_buttom.innerHTML = "😃";
 let counter: number = 0;
-emoji_buttom.addEventListener("click", () => {
+function counter_add() {
   counter++;
   emoji_buttom.innerHTML = `😃+${counter}`;
-});
+}
+emoji_buttom.addEventListener("click", counter_add);
 app.append(emoji_buttom);
+
+//step3
+const auto_click_emoji_buttom = setInterval(counter_add, 1000);
+console.log(auto_click_emoji_buttom);
