9d1c07ddc6ee677dece1e5106aaac9efb49cebc6
nmjxblw <zchen386@ucsc.edu>
2023-10-10 15:18:26 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,22 @@ app.append(emoji_buttom);
 //step3
 const auto_click_emoji_buttom = setInterval(counter_add, 1000);
 console.log(auto_click_emoji_buttom);
+
+//step4
+clearTimeout(auto_click_emoji_buttom);
+function counter_add_by_frame(time: number) {
+  counter += time / 1000;
+  emoji_buttom.innerHTML = `😃+${counter.toFixed(4)}`;
+}
+
+let current_time: number;
+function step(stamptime: number) {
+  if (current_time === undefined) {
+    current_time = stamptime;
+  }
+  let diff = stamptime - current_time;
+  current_time = stamptime;
+  counter_add_by_frame(diff);
+  window.requestAnimationFrame(step);
+}
+window.requestAnimationFrame(step);
