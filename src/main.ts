0b73f40f0ea89a6eb239d34d664ab0bfb3e09b4a
nmjxblw <zchen386@ucsc.edu>
2023-10-10 15:52:39 -0700

    step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
+let growth_rate = 0;
 
 //this line is for test
 const gameName = "Zhuo's game";
@@ -14,11 +15,11 @@ app.append(header);
 
 //step2
 const emoji_buttom = document.createElement("buttom");
-emoji_buttom.innerHTML = "😃";
+emoji_buttom.innerHTML = "👍";
 let counter: number = 0;
 function counter_add() {
   counter++;
-  emoji_buttom.innerHTML = `😃+${counter}`;
+  emoji_buttom.innerHTML = `👍+${counter}`;
 }
 emoji_buttom.addEventListener("click", counter_add);
 app.append(emoji_buttom);
@@ -30,8 +31,8 @@ console.log(auto_click_emoji_buttom);
 //step4
 clearTimeout(auto_click_emoji_buttom);
 function counter_add_by_frame(time: number) {
-  counter += time / 1000;
-  emoji_buttom.innerHTML = `😃+${counter.toFixed(4)}`;
+  counter += (growth_rate * time) / 1000;
+  emoji_buttom.innerHTML = `👍+${counter.toFixed(4)}`;
 }
 
 let current_time: number;
@@ -45,3 +46,22 @@ function step(stamptime: number) {
   window.requestAnimationFrame(step);
 }
 window.requestAnimationFrame(step);
+
+//step5
+const new_line = document.createElement("br");
+app.append(new_line);
+const add_growth_rate_buttom = document.createElement(
+  "buttom",
+) as HTMLButtonElement;
+add_growth_rate_buttom.disabled = true;
+const price: number = 10;
+const growth_rate_per_sec: number = 1;
+add_growth_rate_buttom.innerHTML = `-${price} 👍 -> 1 👍 /s `;
+add_growth_rate_buttom.addEventListener("click", () => {
+  add_growth_rate_buttom.disabled = counter >= price ? false : true;
+  if (!add_growth_rate_buttom.disabled) {
+    counter -= price;
+    growth_rate += growth_rate_per_sec;
+  }
+});
+app.append(add_growth_rate_buttom);
